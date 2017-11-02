f208d9d37fb93fa1765ab756d1ab397b14af9cb6
Savan <Savan777@users.noreply.github.com>
2017-11-02 12:17:09 -0400

    added commnets to line 7&8

--- SQL create table/TweetTable.sql
+++ SQL create table/TweetTable.sql
@@ -4,7 +4,7 @@ Text_ID int NOT NULL PRIMARY KEY,
 User_ID int NOT NULL,
 Message varchar(255) NOT NULL,
 DateCreated DATETIME NOT NULL,
-ST varchar(32) NOT NULL,
-LT varchar(32) NOT NULL,
+ST varchar(32) NOT NULL, -- Source of text
+LT varchar(32) NOT NULL, --Location of text
 FOREIGN KEY(User_ID) REFERENCES Users(User_ID)
 );
