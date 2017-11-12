9acf62ae613230242bf970171a519baa0659f9f3
Savan Patel <savan.patel@dcmail.ca>
2017-11-12 18:11:38 -0500

    cant use that because api does not have that functionality
    twitter api doesnt have ability to get amount of replies to a certain tweet

--- SQL create table/FilterTable.sql
+++ SQL create table/FilterTable.sql
@@ -6,7 +6,6 @@ Country varchar(32) NOT NULL,
 Sentiment varchar(32) NOT NULL,
 Favourites_Count int null,
 Retweet_Count int null,
-Reply_Count int null,
 FOREIGN KEY (Text_ID) REFERENCES Tweet(Text_ID)
 );
 
