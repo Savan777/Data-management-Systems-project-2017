c345391f3c116c9fd72eb7a5fcd67d831cb32536
Savan <Savan777@users.noreply.github.com>
2017-11-22 12:42:06 -0500

    made a new script to get most recent tweets of that user and write it to a csv file
    added populate script to populate the database from the csv file.

--- RawData/getTweet.py
+++ RawData/getTweet.py
@@ -1,11 +1,14 @@
-import tweepy 
+import tweepy
 import json
+import string
+import pandas as pd
+import  datetime
 
 # Twitter API credentials
-consumerKey = "Put your consumer key here"
-consumerSecret = "Put your consumer secret key here"
-tokenKey = "Put your token key here"
-tokenSecret = "Put your token secret key here"
+consumerKey = "CFzT0X0jaOJDXcBTzVtaviY9d"
+consumerSecret = "TyZogohaB74ITobQMFTp5Z1BMwXIfFnoDWF2f7fkmUJ5BRluem"
+tokenKey = "3137614402-qnvVpqY2cDoFjaQqKLvHgt18N6aEikoN3Qn532f"
+tokenSecret = "sZbQNibpzn4PgpBvjcr53bhYnEpRjvA7cZgVK8EbgSEpX"
 
 # Twitter only allows access to a users most recent 3240 tweets with this method
 def get_tweets(screen_name):
@@ -15,43 +18,55 @@ def get_tweets(screen_name):
     auth.set_access_token(tokenKey, tokenSecret)
     api = tweepy.API(auth)
 
-    # initialize a list to hold all the Tweets
-    allTweets = []
+    # make initial request for most recent tweets (max 20)
+    newTweets = api.user_timeline(screen_name=screen_name)
 
-    # make initial request for most recent tweets (200 is the maximum allowed count), getting 100 most recent tweets
-    newTweets = api.user_timeline(screen_name=screen_name, count=100)
+    tweetfile = open("tweets.txt","w")
+#parsing tweet and printing it o txt file
+    tweets_data = []
+    for tweet in newTweets:
+        tweet = get_data(tweet)
 
-    # save most recent tweets
-    allTweets.extend(newTweets)
+        #cleaning the json
+        tweet['displayname'] = filter(lambda x: x in string.printable, tweet['displayname'] )
+        tweet['message'] = filter(lambda x: x in string.printable, tweet['message'])
+        tweet['text_id'] = filter(lambda x: x in string.printable, tweet['text_id'])
+        tweet['st'] = filter(lambda x: x in string.printable, tweet['st'])
+        tweet['lt'] = filter(lambda x: x in string.printable, tweet['lt'])
+        tweet['summary'] = filter(lambda x: x in string.printable, tweet['summary'])
+        tweet['country'] = filter(lambda x: x in string.printable, tweet['country'])
+        tweet['datecreated'] = datetime.datetime.now().date()
 
-    # save the id of the oldest tweet by one less
-    previousTweet = allTweets[-1].id - 1
+        #adding the data to array
+        tweets_data.append(tweet)
 
-    # keep grabbing tweets until limit is reached from initial request
-    while len(newTweets) > 0:
-        # all subsequent requests use the max_id param to prevent duplicates
-        newTweets = api.user_timeline(screen_name=screen_name, count=100, max_id=previousTweet)
+    #creating dataframe
+    data = pd.DataFrame.from_records(tweets_data)
+    data.to_csv('data.csv', index=False) #writing to csv
+    tweetfile.close()
 
-        # save most recent tweets
-        allTweets.extend(newTweets)
+#extracts certain things from the tweet
+def get_data(tweet):
+    data = {}
+    data['displayname'] = tweet.user.screen_name
+    data['message'] = tweet.text
+    data['datecreated'] = tweet.created_at #need to get the time from the time stamp to put in the times column in filter table
+    data['user_id'] = tweet.user.id
+    data['text_id'] = tweet.id_str
+    data['st'] = tweet.source
+    data['lt'] = tweet.user.location
+    data['summary'] = tweet.user.description
+    data['followers_count'] = tweet.user.followers_count
+    data['friends_count'] = tweet.user.friends_count
+    data['profileaddress'] = tweet.user.location
+    data['country'] = tweet.user.time_zone
+    data['favourites_count'] = tweet.favorite_count
+    data['retweet_count'] = tweet.retweet_count
 
-        # update the id of the oldest tweet by one less
-        previousTweet = allTweets[-1].id - 1
+    for hashtag in tweet.entities["hashtags"]:  # iterate over the list
+        data['hashtags'] = (hashtag["text"])
 
-    # write tweet objects to JSON file
-    file = open('tweet.json', 'wb')
-    for status in allTweets:
-        json.dump(status._json, file, sort_keys=True, indent=4)
-
-    # close the file"
-    file.close()
-
-    # # Get the User object for twitter...
-    # user = api.get_user('readDonaldTrump')
-    #
-    # print user.screen_name
-    # print user.followers_count
-
+    return data
 
 if __name__ == '__main__':
     # passing in the username of the account we want to grab tweets from
--- RawData/populate.py
+++ RawData/populate.py
@@ -1,52 +1,28 @@
-import pandas
-import requests
-from sqlalchemy import create_engine
-from urls import urls
+import pandas as pd
+import sqlalchemy
+from sqlalchemy import sql
+from sqlalchemy import create_engine, String, INTEGER, VARCHAR, BIGINT
 
 
 class Populate:
     def __init__(self):
-        # localhost:5432/XXX' where XXX is the name of the databases
-        self.engine = create_engine('postgresql://root:root@localhost:5432/backupTwitter')
+        self.engine = create_engine('postgresql://root:root@localhost:5432/Twitter') #connection to database
 
     def populate(self):
-        # gets players and writes to sql, replacing if table exists
-        players = get_dataframe_from_response(urls[0])
+        #reads data from csv file
+        dataframe = pd.read_csv('data.csv', usecols=['country', 'datecreated', 'displayname', 'favourites_count', 'followers_count', 'friends_count', 'lt', 'profileaddress', 'retweet_count', 'st', 'summary', 'message', 'text_id', 'user_id'])
 
-        # name of SQL table, db connection, ~if table exists, drop it, recreate it and insert data~,
-        # , write DataFrame index as a column true/false
-        players.to_sql('players', con=self.engine, if_exists='replace', index=False)
-        # gets teams and writes to sql, replace if table exists (if you want to append to the table,
-        # use if_exists='append')
+        #selecting required columns from dataframe
+        userframe = dataframe.loc[:, ['user_id', 'displayname', 'summary', 'followers_count', 'friends_count', 'profileaddress']]
+        tweetframe = dataframe.loc[:, ['text_id', 'user_id', 'message', 'datecreated', 'st', 'lt']]
+        filterframe = dataframe.loc[:, ['text_id', 'country', 'favourites_count', 'retweet_count']]
+        hashtagframe = dataframe.loc[:, ['text_id']]
 
-        # Call function below
-        teams = get_dataframe_from_response(urls[1])
+        #writing to database, dtype is datatype dont need it but just put it there cause it works
+        filterframe.to_sql("filter", con=self.engine, if_exists='replace', index=False)
+        hashtagframe.to_sql("hashtag", con=self.engine, if_exists='replace', index=False)
+        tweetframe.to_sql("tweet", con=self.engine, if_exists='replace', index=False, dtype={'text_id': BIGINT, 'user_id': INTEGER, 'message': String, 'datecreated': VARCHAR, 'st': String, 'lt': String})
+        userframe.to_sql("users", con=self.engine, if_exists='replace', index=False, dtype={'user_id': INTEGER, 'displayname': VARCHAR, 'summary': VARCHAR, 'followers_count': INTEGER, 'friends_count': INTEGER, 'profileaddress': VARCHAR})
 
-        # I only want to store certain columns
-        # .loc[] is a label-location based indexer for selection by label
-        # A list or array of labels is given as parameters
-        teams = teams.loc[:, ['TEAM_ID', 'TEAM_NAME']]
-        teams.to_sql('teams', con=self.engine, if_exists='replace', index=False)
-
-def get_dataframe_from_response(url):
-    # sends a request to the url, requests key : value payload
-    # reponse is a decoded text response from the server
-    # custom header is used, given less precedence.
-    response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
-    # any response other than 200 is an error
-    while response.status_code != 200:
-        response = requests.get(url)
-    # if youre dealing with json data then use .json() to decode it
-    headers = response.json()['resultSets'][0]['headers']
-    data = response.json()['resultSets'][0]['rowSet']
-    # data can contain series, array, constants or list-like objects
-    # columns are column labels to use for resulting frame
-    frame = pandas.DataFrame(data, columns=headers)
-
-    return frame
-
-
-# Initialize populate object
 populate = Populate()
-# Call populate function on populate object
 populate.populate()
