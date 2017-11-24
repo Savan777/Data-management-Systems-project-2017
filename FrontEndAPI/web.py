ee6b72db42673d16711945bccb977000509a07fa
Justin K -School <justinjoseph0007@gmail.com>
2017-11-24 13:42:48 -0500

    Modified files with basic api calls

--- FrontEndAPI/web.py
+++ FrontEndAPI/web.py
@@ -15,37 +15,37 @@ CsrfProtect(app)
 # create connection object and get data for teams and players
 db = connection.Connection()
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    class SelectTeamForm(Form):
-        teams = db.get_teams()
-        name = SelectField(coerce=int, choices=teams, default=1610612737L)
-    form = SelectTeamForm()
-    print(form.errors)
-    # handle post request in form
-    if form.validate_on_submit():
-        session['TEAM_ID'] = form.name.data
-        return redirect('/player')
-    return render_template("index.html", form=form)
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     class SelectTeamForm(Form):
+#         teams = db.get_teams()
+#         name = SelectField(coerce=int, choices=teams, default=1610612737L)
+#     form = SelectTeamForm()
+#     print(form.errors)
+#     # handle post request in form
+#     if form.validate_on_submit():
+#         session['TEAM_ID'] = form.name.data
+#         return redirect('/player')
+#     return render_template("index.html", form=form)
 
-@app.route('/player', methods=['GET','POST'])
-def player():
-    class SelectPlayerForm(Form):
-        team_id = session['TEAM_ID']
-        players = db.get_players(team_id)
-        name = SelectField(coerce=int, choices=players)
-    form = SelectPlayerForm()
-    # handle post request in form
-    if form.validate_on_submit():
-        session['PLAYER_ID'] = form.name.data
-        return redirect('/stats')
-    return render_template("player.html", form=form)
+# @app.route('/player', methods=['GET','POST'])
+# def player():
+#     class SelectPlayerForm(Form):
+#         team_id = session['TEAM_ID']
+#         players = db.get_players(team_id)
+#         name = SelectField(coerce=int, choices=players)
+#     form = SelectPlayerForm()
+#     # handle post request in form
+#     if form.validate_on_submit():
+#         session['PLAYER_ID'] = form.name.data
+#         return redirect('/stats')
+#     return render_template("player.html", form=form)
 
-@app.route('/stats', methods=['POST', 'GET'])
-def stats():
-    player_id = session['PLAYER_ID']
-    stats = db.get_stats(player_id)[0]
-    return render_template("stats.html", name=stats[1], blocks=stats[9], drfgm=stats[11], drfga=stats[12], drfgpct=stats[13])
+# @app.route('/stats', methods=['POST', 'GET'])
+# def stats():
+#     player_id = session['PLAYER_ID']
+#     stats = db.get_stats(player_id)[0]
+#     return render_template("stats.html", name=stats[1], blocks=stats[9], drfgm=stats[11], drfga=stats[12], drfgpct=stats[13])
 
 # create simple api that takes in id and response with stats of said player
 # ex http://localhost:5000/api/201960
@@ -53,7 +53,7 @@ def stats():
 @app.route('/api/<id>', methods=['GET','POST'])
 def api(id):
     text_id = id
-    tweets = db.get_stats(text_id)
+    tweets = db.get_tweets(text_id)
     return jsonify(tweets)
 
 if __name__ == '__main__':
